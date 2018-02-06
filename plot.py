d1bcbda9cb13789344939d9c02fed0aef4f5c33d
Chawla Dhruv <chawlad1@teach.cs.toronto.edu>
2018-02-05 23:27:16 -0500

    Produced image for part1

--- plot.py
+++ plot.py
@@ -15,6 +15,8 @@ import cPickle
 import os
 from scipy.io import loadmat
 
+plt.switch_backend('agg')
+
 def plot_each_digit():
     """Save 10 images of each of the 10 digits in the dataset
     """
@@ -26,7 +28,7 @@ def plot_each_digit():
 
     for i in range(10):
         for j in range(10):
-            axarr[i][j].plot(M["train"+str(i)][j].reshape((28,28)), cmap=cm.gray)
+            axarr[i][j].imshow(M["train"+str(i)][j].reshape((28,28)), cmap=cm.gray)
             axarr[i][j].axis('off')
 
     plt.savefig("part1.png")
