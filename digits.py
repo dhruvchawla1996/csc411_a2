3543b26f376f67b1b7dd60a0b8aa1c2d5decfce1
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2018-02-05 23:43:58 -0500

    Make digits.py

---
+++ digits.py
@@ -0,0 +1,31 @@
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import time
+from scipy.misc import imread
+from scipy.misc import imresize
+import matplotlib.image as mpimg
+from scipy.ndimage import filters
+import urllib
+from numpy import random
+
+import cPickle
+
+import os
+from scipy.io import loadmat
+
+from mnist_handouts import *
+from plot import *
+
+################################################################################
+# Part 1
+################################################################################
+def part1():
+	plot_each_digit()
+
+################################################################################
+# Part 2
+################################################################################
+def part2():
+	pass
