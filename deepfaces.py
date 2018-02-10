a9a81163e0a030ea9322b2e91651050dbb1342d7
Chawla Dhruv <chawlad1@teach.cs.toronto.edu>
2018-02-10 18:36:38 -0500

    Get started on part8

---
+++ deepfaces.py
@@ -0,0 +1,109 @@
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import time
+from scipy.misc import imread
+from scipy.misc import imresize
+import matplotlib.image as mpimg
+from scipy.ndimage import filters
+import urllib
+from numpy import random
+
+import cPickle
+
+import os
+from scipy.io import loadmat
+from torch.autograd import Variable
+import torch
+
+from mnist_handout import *
+from plot import *
+from faces import *
+
+################################################################################
+# Part 8
+################################################################################
+# def part8():
+# Actors for training and validation set
+act = ['Lorraine Bracco', 'Peri Gilpin', 'Angie Harmon', 'Alec Baldwin', 'Bill Hader', 'Steve Carell']
+
+# Uncomment if images need to be downloaded in ./cropped/ folder
+# If it doesn't work, unzip cropped.zip
+############################################################################
+# get_and_crop_images(act)
+# remove_bad_images()
+############################################################################
+
+train_set, train_label = np.zeros((0, 32*32)), np.zeros((0, len(act)))
+test_set, test_label = np.zeros((0, 32*32)), np.zeros((0, len(act)))
+
+for i in range(len(act)):
+    a_name = act[i].split()[1].lower()
+
+    train_set_i, test_set_i = build_sets(a_name)
+
+    one_hot = np.zeros(len(act))
+    one_hot[i] = 1
+
+    train_set = np.vstack((train_set, train_set_i))
+    test_set = np.vstack((test_set, test_set_i))
+
+    train_label = np.vstack((train_label, np.tile(one_hot, (train_set_i.shape[0], 1))))
+    test_label = np.vstack((test_label, np.tile(one_hot, (test_set_i.shape[0], 1))))
+
+dim_x = 32*32
+dim_h = 20
+dim_out = len(act)
+
+dtype_float = torch.FloatTensor
+dtype_long = torch.LongTensor
+
+x = Variable(torch.from_numpy(train_set[:]), requires_grad=False).type(dtype_float)
+y_classes = Variable(torch.from_numpy(np.argmax(train_label[:], 1)), requires_grad=False).type(dtype_long)
+
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(dim_x, dim_h),
+    torch.nn.ReLU(),
+    torch.nn.Linear(dim_h, dim_out),
+)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+
+learning_rate = 1e-2
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+print("Here")
+for t in range(10000):
+    y_pred = model(x)
+    loss = loss_fn(y_pred, y_classes)
+    
+    model.zero_grad()  # Zero out the previous gradient computation
+    loss.backward()    # Compute the gradient
+    optimizer.step()   # Use the gradient information to 
+                       # make a step
+
+x = Variable(torch.from_numpy(test_set), requires_grad=False).type(dtype_float)
+
+y_pred = model(x).data.numpy()
+
+print np.mean(np.argmax(y_pred, 1) == np.argmax(test_label, 1))
+
+################################################################################
+# Part 9
+################################################################################
+def part9():
+    pass
+
+################################################################################
+# Part 10
+################################################################################
+def part10():
+    pass
+
+################################################################################
+# Function calls
+################################################################################
+# part8()
+# part9()
+# part10()
---
+++ faces.py
@@ -0,0 +1,185 @@
+# Imports
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+import random
+import time
+from scipy.misc import imread
+from scipy.misc import imresize
+import matplotlib.image as mpimg
+import os
+from scipy.ndimage import filters
+import urllib
+import hashlib
+import shutil
+
+################################################################################
+# Downloading and populating images from the dataset
+################################################################################
+def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
+    '''From:
+    http://code.activestate.com/recipes/473878-timeout-function-using-threading/
+    Manages download by aborting download if it's taking too long'''
+    import threading
+    class InterruptableThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.result = None
+
+        def run(self):
+            try:
+                self.result = func(*args, **kwargs)
+            except:
+                self.result = default
+
+    it = InterruptableThread()
+    it.start()
+    it.join(timeout_duration)
+    if it.isAlive():
+        return False
+    else:
+        return it.result
+
+testfile = urllib.URLopener()            
+
+
+def get_and_crop_images(act):
+    '''Downloads images from faces_subset.txt
+    Stores raw images into ./uncropped and processes them into 32x32 grayscale images in ./cropped
+
+    Requires: faces_subset.txt as obtained from the FaceScrub dataset
+                act: list of actor names
+
+    Stores images as lastname_i.jpg. Ex: giplin1.jpg
+    '''
+    # Remove folders cropped/ and uncropped/
+    if os.path.exists('./cropped'): shutil.rmtree('./cropped')
+    if os.path.exists('./uncropped'): shutil.rmtree('./uncropped')
+
+    # Create cropped/ and uncropped/
+    if not os.path.exists('./cropped'): os.makedirs('cropped')
+    if not os.path.exists('./uncropped'): os.makedirs('uncropped')
+
+    for a in act:
+        name = a.split()[1].lower()
+        i = 0
+        for line in open("faces_subset.txt"):
+            if a in line:
+                filename = name+str(i)+'.'+line.split()[4].split('.')[-1]
+                #A version without timeout (uncomment in case you need to
+                #unsupress exceptions, which timeout() does)
+                #testfile.retrieve(line.split()[4], "uncropped/"+filename)
+                #timeout is used to stop downloading images which take too long to download
+                timeout(testfile.retrieve, (line.split()[4], "uncropped/"+filename), {}, 45)
+
+                crop_bbox = line.split()[5].split(',')
+                sha256_hash = line.split()[6]
+
+                # Convert uncropped image to cropped 32x32 grayscale
+                if not os.path.isfile("uncropped/"+filename):
+                    continue
+
+                try:
+                    rgb_img = imread("uncropped/"+filename)
+                    grayscale_img = rgb2gray(rgb_img)
+                    cropped_img = grayscale_img[int(crop_bbox[1]):int(crop_bbox[3]), int(crop_bbox[0]):int(crop_bbox[2])]
+                    resized_img = imresize(cropped_img, (32, 32))
+                    imsave("cropped/"+filename, resized_img, cmap = plt.cm.gray)
+
+                except Exception as e:
+                    print(str(e))
+
+                print filename
+                i += 1
+
+def remove_bad_images():
+    '''Removes bad images from list (manually chosen)
+    Requires: cropped images in ./cropped
+    '''
+    bad_image_filenames = ["baldwin68",
+                            "bracco90",
+                            "bracco64",
+                            "butler131",
+                            "butler132",
+                            "carell93",
+                            "chenoweth29",
+                            "chenoweth80",
+                            "chenoweth94",
+                            "drescher106",
+                            "drescher109",
+                            "drescher124",
+                            "drescher88",
+                            "ferrera159",
+                            "ferrera123",
+                            "hader4",
+                            "hader63",
+                            "hader77",
+                            "hader98",
+                            "harmon50",
+                            "radcliffe28",
+                            "vartan59",
+                            "vartan72"]
+
+    for filename in bad_image_filenames:
+        if os.path.isfile("cropped/"+filename+".jpg"):
+            os.remove("cropped/"+filename+".jpg")
+
+################################################################################
+# Building training, validation and testing sets for an actor
+################################################################################
+def build_sets(actor):
+    '''Return two lists of randomized image names 
+    in cropped/ folder that match actor name
+    
+    Training Set - At least 67 image names (Screw Peri Gilpin)
+    Validation Set - 10 image names 
+    Testing Set - 10 image names
+    
+    Takes in name as lowercase last name (ex: gilpin)
+
+    Assumption: cropped/ folder is populated with images from get_and_crop_images
+    '''
+    # Make a list of images for the actor
+    image_list = []
+
+    for f in os.listdir("cropped"):
+        if actor in f:
+            image_list.append(f)
+
+    # Shuffle
+    np.random.seed(5)
+    np.random.shuffle(image_list)
+
+    train_set = np.zeros((0, 32*32))
+    test_set = np.zeros((0, 32*32))
+
+    for img in image_list[:20]:
+        t_img = imread("cropped/"+img)
+        t_img = rgb2gray(t_img)
+        t_img = reshape(np.ndarray.flatten(t_img), [1, 32*32])
+        test_set = np.vstack((test_set, t_img))
+
+    for img in image_list[20:]:
+        tr_img = imread("cropped/"+img)
+        tr_img = rgb2gray(tr_img)
+        tr_img = reshape(np.ndarray.flatten(tr_img), [1, 32*32])
+        train_set = np.vstack((train_set, tr_img))
+
+    return train_set, test_set
+
+################################################################################
+# Converting images to grayscale
+################################################################################
+def rgb2gray(rgb):
+    '''Return the grayscale version of the RGB image rgb as a 2D numpy array
+    whose range is 0..1
+    Arguments:
+    rgb -- an RGB image, represented as a numpy array of size n x m x 3. The
+    range of the values is 0..255
+    '''
+    
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray/255.
