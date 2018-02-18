8959f137f1dd9b2df4b289553822e300921e4e41
Sabrina Lokman <35489261+sabreenalo@users.noreply.github.com>
2018-02-18 02:08:42 -0500

    Part 5 & part 6 update
    Not final yet, but this is what I have so far. Will work on this more on Sunday evening.

--- digits.py
+++ digits.py
@@ -80,54 +80,90 @@ def part3():
 ################################################################################
 # Part 4
 ################################################################################
-# def part4():
-#Load the MNIST digit data
-M = loadmat("mnist_all.mat")
+def part4():
+    #Load the MNIST digit data
+    M = loadmat("mnist_all.mat")
 
-# Split data into training and test set
-train_set, train_label = np.zeros((0, 28*28)), np.zeros((0, 10))
-test_set, test_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+    # Split data into training and test set
+    train_set, train_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+    test_set, test_label = np.zeros((0, 28*28)), np.zeros((0, 10))
 
-for i in range(10):
-    train_set = np.vstack((train_set, ((np.array(M["train"+str(i)])[:])/255.)))
-    test_set = np.vstack((test_set, ((np.array(M["test"+str(i)])[:])/255.)))
+    for i in range(10):
+        train_set = np.vstack((train_set, ((np.array(M["train"+str(i)])[:])/255.)))
+        test_set = np.vstack((test_set, ((np.array(M["test"+str(i)])[:])/255.)))
 
-    one_hot = np.zeros(10)
-    one_hot[i] = 1
+        one_hot = np.zeros(10)
+        one_hot[i] = 1
 
-    train_label = np.vstack((train_label, np.tile(one_hot, (len(M["train"+str(i)]), 1))))
-    test_label = np.vstack((test_label, np.tile(one_hot, (len(M["test"+str(i)]), 1))))
+        train_label = np.vstack((train_label, np.tile(one_hot, (len(M["train"+str(i)]), 1))))
+        test_label = np.vstack((test_label, np.tile(one_hot, (len(M["test"+str(i)]), 1))))
 
-train_set, train_label, test_set, test_label = train_set.T, train_label.T, test_set.T, test_label.T
+    train_set, train_label, test_set, test_label = train_set.T, train_label.T, test_set.T, test_label.T
 
-# Load weights and biases
-snapshot = cPickle.load(open("snapshot50.pkl"))
-W0 = snapshot["W0"]
-b0 = snapshot["b0"].reshape((300,1))
-W1 = snapshot["W1"]
-b1 = snapshot["b1"].reshape((10,1))
+    # Load weights and biases
+    snapshot = cPickle.load(open("snapshot50.pkl"))
+    W0 = snapshot["W0"]
+    b0 = snapshot["b0"].reshape((300,1))
+    W1 = snapshot["W1"]
+    b1 = snapshot["b1"].reshape((10,1))
 
-init_W = np.dot(W0, W1)
-init_b = b1
+    init_W = np.dot(W0, W1)
+    init_b = b1
 
-alpha = 0.00001
+    alpha = 0.00001
 
-W, b, epoch, train_perf, test_perf = train_nn(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
+    W, b, epoch, train_perf, test_perf = train_nn(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
 
-plot_learning_curves("part4", epoch, train_perf, test_perf)
-plot_digit_weights(W)
+    plot_learning_curves("part4", epoch, train_perf, test_perf)
+    plot_digit_weights(W)
 
 ################################################################################
 # Part 5
 ################################################################################
 def part5():
-    pass
+    #Load the MNIST digit data
+    M = loadmat("mnist_all.mat")
+
+    # Split data into training and test set
+    train_set, train_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+    test_set, test_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+
+    for i in range(10):
+        train_set = np.vstack((train_set, ((np.array(M["train"+str(i)])[:])/255.)))
+        test_set = np.vstack((test_set, ((np.array(M["test"+str(i)])[:])/255.)))
+
+        one_hot = np.zeros(10)
+        one_hot[i] = 1
+
+        train_label = np.vstack((train_label, np.tile(one_hot, (len(M["train"+str(i)]), 1))))
+        test_label = np.vstack((test_label, np.tile(one_hot, (len(M["test"+str(i)]), 1))))
+
+    train_set, train_label, test_set, test_label = train_set.T, train_label.T, test_set.T, test_label.T
+
+    # Load weights and biases
+    snapshot = cPickle.load(open("snapshot50.pkl"))
+    W0 = snapshot["W0"]
+    b0 = snapshot["b0"].reshape((300,1))
+    W1 = snapshot["W1"]
+    b1 = snapshot["b1"].reshape((10,1))
+
+    init_W = np.dot(W0, W1)
+    init_b = b1
+
+    alpha = 0.00001
+
+    W, b, epoch, train_perf, test_perf = train_nn_M(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
+
+    plot_learning_curves("part5", epoch, train_perf, test_perf)
+    plot_digit_weights(W)
 
 ################################################################################
 # Part 6
 ################################################################################
 def part6():
-    pass
+
+    plot_trajectory("part6", mo_traj, w1, w2)
+    
 
 ################################################################################
 # Function calls
