2c4e473f0bc38053ac9f9ac5d97e31bbd7cfa42a
Saila <saila.sms96@gmail.com>
2018-02-22 17:54:58 -0500

    updated gradient function, finite differences method and training with momentum function

--- digits.py
+++ digits.py
@@ -64,6 +64,8 @@ def part3():
     W = np.dot(W0, W1)
 
     # Construct a sample output (all elements sum up to unity)
+    #TODO: push this change, random.seed
+    np.random.seed(5)
     y = np.random.rand(10, 1)
     y /= sum(y)
 
@@ -71,11 +73,70 @@ def part3():
     h = 0.001
 
 
-    print("Checking Difference with respect to weights: \n")
-    check_finite_differences_w(x, W, b1, y, h)
+    grad_mat = gradient_simple_network_b(x, W, b1, y)
+    print(grad_mat.shape)
+    # print("Checking Difference with respect to weights: \n")
+    # check_finite_differences_w(x, W, b1, y, h)
+    #
+    # print("\nChecking Difference with respect to biases: \n")
+    # check_finite_differences_b(x, W, b1, y, h)
 
-    print("\nChecking Difference with respect to biases: \n")
-    check_finite_differences_b(x, W, b1, y, h)
+
+def part3_rerun():
+    np.random.seed(5)
+
+    #Load the MNIST digit data
+    M = loadmat("mnist_all.mat")
+
+    # Load an image
+    x = M["train5"][150].reshape((28*28, 1))
+    x = x/255.0
+
+    # Load weights and biases
+    snapshot = cPickle.load(open("snapshot50.pkl"))
+    W0 = snapshot["W0"]
+    b0 = snapshot["b0"].reshape((300,1))
+    W1 = snapshot["W1"]
+    b1 = snapshot["b1"].reshape((10,1))
+
+    W = np.dot(W0, W1)
+
+    # Construct a sample output (all elements sum up to unity)
+    #TODO: push this change, random.seed
+    np.random.seed(5)
+    y = np.random.rand(10, 1)
+    y /= sum(y)
+
+    # h value
+    h = 0.001
+
+    #testing whether it's same as previous gradient function
+    # grad_mat_W_original = gradient_simple_network_w(x, W, b1, y)
+    grad_mat_W_modified = gradient_simple_network_w_2(x, W, b1, y)
+    # print(grad_mat_W_original.shape, grad_mat_W_modified.shape)
+    # print(grad_mat_W_modified == grad_mat_W_original)
+
+    #TODO: choose nonzero coordinates
+    coords = np.zeros((2, 50))
+    np.random.seed(5000)
+    random_is = np.random.randint(0, 784, (1, coords.shape[1]))
+    np.random.seed(5001)
+    random_js = np.random.randint(0, 10, (1, coords.shape[1]))
+    for i in range(coords.shape[1]):
+        random_i = random_is[0][i]
+        random_j = random_js[0][i]
+        coords[:, i] = np.array([random_i, random_j])
+
+    # for i in range(coords.shape[1]):
+    #     print(grad_mat_W_modified[int(coords[0,i]), int(coords[1,i])])
+
+    check_grad_w(x, W, b1, y, h, coords)
+
+    # print("Checking Difference with respect to weights: \n")
+    # check_finite_differences_w(x, W, b1, y, h)
+    #
+    # print("\nChecking Difference with respect to biases: \n")
+    # check_finite_differences_b(x, W, b1, y, h)
 
 ################################################################################
 # Part 4
@@ -117,6 +178,45 @@ def part4():
     plot_learning_curves("part4", epoch, train_perf, test_perf)
     plot_digit_weights(W)
 
+
+def part4_rerun():
+    #TODO: rerun part 4 with vectorized gradient
+    #Load the MNIST digit data
+    M = loadmat("mnist_all.mat")
+
+    # Split data into training and test set
+    train_set, train_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+    test_set, test_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+
+    for i in range(10):
+        train_set = np.vstack((train_set, ((np.array(M["train"+str(i)])[:])/255.)))
+        test_set = np.vstack((test_set, ((np.array(M["test"+str(i)])[:])/255.)))
+
+        one_hot = np.zeros(10)
+        one_hot[i] = 1
+
+        train_label = np.vstack((train_label, np.tile(one_hot, (len(M["train"+str(i)]), 1))))
+        test_label = np.vstack((test_label, np.tile(one_hot, (len(M["test"+str(i)]), 1))))
+
+    train_set, train_label, test_set, test_label = train_set.T, train_label.T, test_set.T, test_label.T
+
+    # Load weights and biases
+    snapshot = cPickle.load(open("snapshot50.pkl"))
+    W0 = snapshot["W0"]
+    b0 = snapshot["b0"].reshape((300,1))
+    W1 = snapshot["W1"]
+    b1 = snapshot["b1"].reshape((10,1))
+
+    init_W = np.dot(W0, W1)
+    init_b = b1
+
+    alpha = 0.00001
+
+    W, b, epoch, train_perf, test_perf = train_nn(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
+
+    plot_learning_curves_rerun("part4", epoch, train_perf, test_perf)
+    plot_digit_weights_rerun(W)
+
 ################################################################################
 # Part 5
 ################################################################################
@@ -154,8 +254,52 @@ def part5():
 
     W, b, epoch, train_perf, test_perf = train_nn_M(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
 
-    plot_learning_curves("part5", epoch, train_perf, test_perf)
+    print("plotting")
+    plot_learning_curves("part5", epoch,train_perf, test_perf)
     plot_digit_weights(W)
+
+
+def part5_rerun():
+    '''
+    #TODO test part_5 after testing part4 and checking parameter update for momentum
+    '''
+
+    #Load the MNIST digit data
+    M = loadmat("mnist_all.mat")
+
+    # Split data into training and test set
+    train_set, train_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+    test_set, test_label = np.zeros((0, 28*28)), np.zeros((0, 10))
+
+    for i in range(10):
+        train_set = np.vstack((train_set, ((np.array(M["train"+str(i)])[:])/255.)))
+        test_set = np.vstack((test_set, ((np.array(M["test"+str(i)])[:])/255.)))
+
+        one_hot = np.zeros(10)
+        one_hot[i] = 1
+
+        train_label = np.vstack((train_label, np.tile(one_hot, (len(M["train"+str(i)]), 1))))
+        test_label = np.vstack((test_label, np.tile(one_hot, (len(M["test"+str(i)]), 1))))
+
+    train_set, train_label, test_set, test_label = train_set.T, train_label.T, test_set.T, test_label.T
+
+    # Load weights and biases
+    snapshot = cPickle.load(open("snapshot50.pkl"))
+    W0 = snapshot["W0"]
+    b0 = snapshot["b0"].reshape((300,1))
+    W1 = snapshot["W1"]
+    b1 = snapshot["b1"].reshape((10,1))
+
+    init_W = np.dot(W0, W1)
+    init_b = b1
+
+    alpha = 0.00001
+
+    W, b, epoch, train_perf, test_perf = train_nn_M(compute_simple_network, gradient_simple_network_w, gradient_simple_network_b, train_set, train_label, test_set, test_label, init_W, init_b, alpha)
+
+    print("plotting")
+    plot_learning_curves_rerun("part5", epoch,train_perf, test_perf)
+    plot_digit_weights_rerun(W)
 
 ################################################################################
 # Part 6
@@ -168,9 +312,9 @@ def part6():
 ################################################################################
 # Function calls
 ################################################################################
-# part1()
-# part2()
-# part3()
+#part1()
+#part2()
+part3_rerun()
 # part4()
-# part5()
+#part5()
 # part6()
--- mnist_handout.py
+++ mnist_handout.py
@@ -31,27 +31,27 @@ def softmax(y):
     is the number of cases'''
     return exp(y)/tile(sum(exp(y),0), (len(y),1))
     
-def tanh_layer(y, W, b):    
-    '''Return the output of a tanh layer for the input matrix y. y
-    is an NxM matrix where N is the number of inputs for a single case, and M
-    is the number of cases'''
-    return tanh(dot(W.T, y)+b)
-
-def forward(x, W0, b0, W1, b1):
-    L0 = tanh_layer(x, W0, b0)
-    L1 = dot(W1.T, L0) + b1
-    output = softmax(L1)
-    return L0, L1, output
-    
+# def tanh_layer(y, W, b):
+#     '''Return the output of a tanh layer for the input matrix y. y
+#     is an NxM matrix where N is the number of inputs for a single case, and M
+#     is the number of cases'''
+#     return tanh(dot(W.T, y)+b)
+#
+# def forward(x, W0, b0, W1, b1):
+#     L0 = tanh_layer(x, W0, b0)
+#     L1 = dot(W1.T, L0) + b1
+#     output = softmax(L1)
+#     return L0, L1, output
+#
 def NLL(y, y_):
-    return -sum(y_*log(y)) 
+    return -sum(y_*log(y))
+#
+# def deriv_multilayer(W0, b0, W1, b1, x, L0, L1, y, y_):
+#     '''Incomplete function for computing the gradient of the cross-entropy
+#     cost function w.r.t the parameters of a neural network'''
+#     dCdL1 =  y - y_
+#     dCdW1 =  dot(L0, dCdL1.T )
 
-def deriv_multilayer(W0, b0, W1, b1, x, L0, L1, y, y_):
-    '''Incomplete function for computing the gradient of the cross-entropy
-    cost function w.r.t the parameters of a neural network'''
-    dCdL1 =  y - y_
-    dCdW1 =  dot(L0, dCdL1.T ) 
-    
 def compute_simple_network(x, W, b):
     '''Compute a simple network (with no hidden layers)
     '''
@@ -68,11 +68,30 @@ def gradient_simple_network_w(x, W, b, y):
 
     return gradient_mat
 
+
 def gradient_simple_network_b(x, W, b, y):
     p = compute_simple_network(x, W, b)
 
     return np.sum((p - y), axis=1).reshape((10, 1))
 
+
+def gradient_simple_network_w_2(x, W, b, y):
+    '''
+
+    :param x: shape (n, m)
+    :param W: shape (n, k)
+    :param b: shape(k, m)
+    :param y: shape(k,m)
+           p: shape(k, m)
+    :return: gradient w.r.t to weights of shape (n, k) where n = number of features (pixels) and k = number of output classes
+    '''
+    p = compute_simple_network(x, W, b)
+#    print(x.shape, W.shape, b.shape, y.shape, p.shape)
+    p_minus_y = np.subtract(p, y)
+    gradient_mat = np.matmul(x, p_minus_y.T)
+    return gradient_mat
+
+#TODO: disturb only one weight
 def check_finite_differences_w(x, W, b, y, h):
     for j in range(28*28):
         for i in range(10):
@@ -90,6 +109,26 @@ def check_finite_differences_w(x, W, b, y, h):
                 print("Index: " + str(j) + ", " + str(i))
                 print("Actual Gradient Value:   " + str(actual_grad))
                 print("Finite Difference Value: " + str(finite_diff_grad) + "\n")
+
+
+def check_grad_w(x, W, b, y, h,coords):
+
+    for i in range(coords.shape[1]):
+
+        W_disturbed = W.copy()
+        W_disturbed[int(coords[0,i]), int(coords[1,i])] += h
+
+        actual_grad = gradient_simple_network_w(x, W, b, y)[int(coords[0,i]), int(coords[1,i])]
+
+        cost_fn = NLL(compute_simple_network(x, W, b), y)
+        cost_fn_h = NLL(compute_simple_network(x, W_disturbed, b), y)
+        finite_diff_grad = (cost_fn_h - cost_fn) / h
+
+        if actual_grad != 0:
+            print("Index: " + str(coords[0,i]) + ", " + str(coords[1,i]))
+            print("Actual Gradient Value:   " + str(actual_grad))
+            print("Finite Difference Value: " + str(finite_diff_grad) + "\n")
+
 
 def check_finite_differences_b(x, W, b, y, h):
     for i in range(10):
@@ -155,6 +194,91 @@ def train_nn(f, df_W, df_b, x_train, y_train, x_test, y_test, init_W, init_b, al
         itr += 1
 
     return W, b, epoch, train_perf, test_perf
+
+
+def train_nn_M(f, df_W, df_b, x_train, y_train, x_test, y_test, init_W, init_b, alpha, gamma = 0.9, max_iter = 2000):
+    x = x_train
+    y = y_train
+
+    epoch, train_perf, test_perf = [], [], []
+
+    EPS = 1e-10
+    prev_W = init_W - 10 * EPS
+    prev_b = init_b - 10 * EPS
+    W = init_W.copy()
+    b = init_b.copy()
+    itr = 0
+
+    while norm(W - prev_W) > EPS and norm(b - prev_b) > EPS and itr < max_iter:
+        prev_W = W.copy()
+        prev_b = b.copy()
+
+        W = (gamma * v) - alpha * df_W(x, W, b, y)
+        b -= alpha * df_b(x, W, b, y)
+
+        if itr % 200 == 0 or itr == max_iter - 1:
+            epoch_i = itr
+            train_perf_i = performance(x_train, W, b, y_train)
+            test_perf_i = performance(x_test, W, b, y_test)
+
+            epoch.append(epoch_i)
+            train_perf.append(train_perf_i)
+            test_perf.append(test_perf_i)
+
+            print("Epoch: " + str(epoch_i))
+            print("Training Performance:   " + str(train_perf_i) + "%")
+            print("Testing Performance:    " + str(test_perf_i) + "%\n")
+
+        itr += 1
+
+    return W, b, epoch, train_perf, test_perf
+
+def train_nn_M2(f, df_W, df_b, x_train, y_train, x_test, y_test, init_W, init_b, alpha, gamma = 0.9, max_iter = 2000):
+    '''
+    uses momentum differently than train_nn_M
+    '''
+    x = x_train
+    y = y_train
+
+    epoch, train_perf, test_perf = [], [], []
+
+    EPS = 1e-10
+    prev_W = init_W - 10 * EPS
+    prev_b = init_b - 10 * EPS
+    W = init_W.copy()
+    b = init_b.copy()
+    itr = 0
+    v_W = 0
+    v_b = 0
+
+    while norm(W - prev_W) > EPS and norm(b - prev_b) > EPS and itr < max_iter:
+        prev_W = W.copy()
+        prev_b = b.copy()
+
+        #update velocities
+        v_W = gamma * v_W + alpha * df_W(x,W,b,y)
+        v_b = gamma * v_b + alpha * df_b(x,W,b,y)
+        #update parameters with momentum
+        W = W - v_W
+        b = b - v_b
+
+        if itr % 50 == 0 or itr == max_iter - 1:
+            epoch_i = itr
+            train_perf_i = performance(x_train, W, b, y_train)
+            test_perf_i = performance(x_test, W, b, y_test)
+
+            epoch.append(epoch_i)
+            train_perf.append(train_perf_i)
+            test_perf.append(test_perf_i)
+
+            print("Epoch: " + str(epoch_i))
+            print("Training Performance:   " + str(train_perf_i) + "%")
+            print("Testing Performance:    " + str(test_perf_i) + "%\n")
+
+        itr += 1
+
+    return W, b, epoch, train_perf, test_perf
+
 
 ################################################################################
 # #Load sample weights for the multilayer neural network
